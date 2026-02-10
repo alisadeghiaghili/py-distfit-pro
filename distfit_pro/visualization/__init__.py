72da56987e787f3c45ec8e8f57b088cdc19186a7
Ali Sadeghi Aghili <alisadeghiaghili@gmail.com>
2026-02-10 21:30:41 +0330

    Update visualization __init__ to export DistributionPlotter

--- distfit_pro/visualization/__init__.py
+++ distfit_pro/visualization/__init__.py
@@ -1,1 +1,10 @@
-"""Visualization modules"""
+"""
+Visualization Module
+====================
+
+نمودارهای تشخیصی و مقایسه‌ای
+"""
+
+from .plots import DistributionPlotter
+
+__all__ = ['DistributionPlotter']
