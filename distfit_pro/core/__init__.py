c08b13e605fbea48660edcdb78a3650ebdf6f212
Ali Sadeghi Aghili <alisadeghiaghili@gmail.com>
2026-02-13 00:44:09 +0330

    feat: add core module initialization

--- distfit_pro/core/__init__.py
+++ distfit_pro/core/__init__.py
@@ -1,3 +1,33 @@
-"""Core modules"""
-from .distributions import *
-from .model_selection import *
+"""
+Core Distribution Framework
+===========================
+
+Core components for distribution fitting.
+"""
+
+from .base import (
+    BaseDistribution,
+    ContinuousDistribution,
+    DiscreteDistribution,
+    DistributionInfo,
+    FittingMethod,
+)
+
+from .distributions import (
+    NormalDistribution,
+    ExponentialDistribution,
+    get_distribution,
+    list_distributions,
+)
+
+__all__ = [
+    "BaseDistribution",
+    "ContinuousDistribution",
+    "DiscreteDistribution",
+    "DistributionInfo",
+    "FittingMethod",
+    "NormalDistribution",
+    "ExponentialDistribution",
+    "get_distribution",
+    "list_distributions",
+]
