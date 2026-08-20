675344568b3e57fb78af86c4bf7ceba426d5447d
alisadeghiaghili <alisadeghiaghili@gmail.com>
2026-08-20 12:59:11 +0330

    feat: implement data source preflight planning

--- python/src/veridist/engine/__init__.py
+++ python/src/veridist/engine/__init__.py
@@ -1,1 +1,19 @@
-"""Reserved namespace for the v1 fitting engine."""
+"""The v1 fitting-engine public surface."""
+
+from veridist.engine.data_source import (
+    CheckpointMetadata,
+    DataSourceCapabilityError,
+    DataSourceMetadata,
+    Replayability,
+    SpoolPolicy,
+    plan_passes,
+)
+
+__all__ = [
+    "CheckpointMetadata",
+    "DataSourceCapabilityError",
+    "DataSourceMetadata",
+    "Replayability",
+    "SpoolPolicy",
+    "plan_passes",
+]
